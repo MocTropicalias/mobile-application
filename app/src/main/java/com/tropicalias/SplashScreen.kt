6ad65e8311b35e662d6491f007b959fa895fcbb8
Rutra <artur.cdj@gmail.com>
2024-08-29 12:22:27 -0300

    feat: basic login layout

--- app/src/main/java/com/tropicalias/SplashScreen.kt
+++ app/src/main/java/com/tropicalias/SplashScreen.kt
@@ -1,20 +1,34 @@
 package com.tropicalias
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        val isLoggedIn = getLogin()
+
         GlobalScope.launch(Dispatchers.Main) {
             delay(2000)
-            intent = Intent(this@SplashScreen, MainActivity::class.java)
+            intent = if (isLoggedIn) {
+                Intent(this@SplashScreen, MainActivity::class.java)
+            } else {
+                Intent(this@SplashScreen, LoginActivity::class.java)
+            }
             startActivity(intent)
             finish()
         }
     }
+
+    private fun getLogin(): Boolean {
+        return false
+    }
+
 }
