75f374f3400f721396f0c0c0acdddcd489a55baa
Rutra <artur.cdj@gmail.com>
2024-10-11 20:47:28 -0300

    feat: edit profile (incomplete)

--- app/src/main/java/com/tropicalias/ui/profile/edit/EditProfileFragment.kt
+++ app/src/main/java/com/tropicalias/ui/profile/edit/EditProfileFragment.kt
@@ -1,37 +1,94 @@
 package com.tropicalias.ui.profile.edit
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.google.firebase.auth.FirebaseAuth
-import com.tropicalias.AuthenticationActivity
-import com.tropicalias.databinding.FragmentSettingsBinding
+import com.tropicalias.adapter.ProfileAdapter
+import com.tropicalias.databinding.FragmentEditProfileBinding
+import com.tropicalias.utils.Utils
+import com.wajahatkarim3.easyflipviewpager.CardFlipPageTransformer2
 
 class EditProfileFragment : Fragment() {
 
-    private var _binding: FragmentSettingsBinding? = null
+    private var _binding: FragmentEditProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: ProfileAdapter
+    private val viewModel: EditProfileViewModel by viewModels()
+    lateinit var TAG: String
+
+    private val pickImageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val imageUri = data?.data
+                val finalUri = imageUri ?: adapter.imageUri
+                finalUri?.let {
+//                    Toast.makeText(requireContext(), "Salvando imagem", Toast.LENGTH_SHORT).show()
+//                    viewModel.uploadImageToFirebase(finalUri)
+                    adapter.imageUri = finalUri
+                    adapter.notifyDataSetChanged()
+                }
+            }
+        }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.logoutButton.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            val intent = Intent(requireContext(), AuthenticationActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-            requireActivity().finish()
+        // Get ViewPager2 and Set Adapter
+        adapter = ProfileAdapter(pickImageLauncher, requireContext())
+        binding.viewPager.setAdapter(adapter)
+        binding.viewPager.setCurrentItem(Int.MAX_VALUE / 2, false)
+        // Create an object of page transformer
+        val cardFlipPageTransformer = CardFlipPageTransformer2()
+        cardFlipPageTransformer.isScalable = true
+        // Assign the page transformer to the ViewPager2.
+        binding.viewPager.setPageTransformer(cardFlipPageTransformer)
+
+        val yourUser = FirebaseAuth.getInstance().currentUser
+        //🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄
+        adapter.imageUri = yourUser?.photoUrl
+        adapter.notifyDataSetChanged()
+        binding.usernameEditText.setText(yourUser?.displayName)
+        viewModel.loadUser(binding, adapter)
+
+
+
+        binding.saveButton.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            val username = binding.usernameEditText.text.toString()
+            val bio = binding.bioEditText.text.toString()
+            binding.loadingButton.visibility = View.VISIBLE
+            binding.saveButton.text = ""
+
+            Utils.checkInputsEditProfile(name, username, bio, binding, requireContext()) { error ->
+                if (!error) {
+                    viewModel.saveUpdates(name, username, bio, adapter.imageUri, requireContext())
+                    requireActivity().onBackPressed()
+                }
+                binding.loadingButton.visibility = View.GONE
+                binding.saveButton.text = "Salvar"
+            }
+        }
+
+
+        binding.backButton.setOnClickListener {
+            requireActivity().onBackPressed()
         }
     }
 
---
+++ app/src/main/java/com/tropicalias/ui/profile/edit/EditProfileViewModel.kt
@@ -0,0 +1,73 @@
+package com.tropicalias.ui.profile.edit
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.userProfileChangeRequest
+import com.tropicalias.adapter.ProfileAdapter
+import com.tropicalias.api.model.User
+import com.tropicalias.api.repository.ApiRepository
+import com.tropicalias.databinding.FragmentEditProfileBinding
+import com.tropicalias.utils.Utils
+import retrofit2.Call
+import retrofit2.Response
+
+class EditProfileViewModel : ViewModel() {
+
+    private val repository = ApiRepository.getInstance()
+    val user = repository.user
+    val apiSQL = repository.getSQL()
+
+    fun loadUser(binding: FragmentEditProfileBinding, adapter: ProfileAdapter) {
+        binding.loading.visibility = View.VISIBLE
+        binding.loading.setOnClickListener { }
+        Utils.getUser { user ->
+            binding.loading.visibility = View.GONE
+            adapter.imageUri = user.imageUri
+            adapter.notifyDataSetChanged()
+            binding.nameEditText.setText(user.exibitionName)
+            binding.usernameEditText.setText(user.username)
+            binding.bioEditText.setText(user.userDescription)
+        }
+    }
+
+    fun saveUpdates(name: String, username: String, bio: String, image: Uri?, con: Context) {
+
+        user.value?.exibitionName = name
+        user.value?.username = username
+        user.value?.userDescription = bio
+        user.value?.imageUri = image
+
+        user.value?.let { user ->
+            Log.d("EDIT PROFILE", "saveUpdates: $user")
+            apiSQL.updateUserProfile(user, user.id!!.toString())
+                .enqueue(object : retrofit2.Callback<User> {
+                    override fun onResponse(req: Call<User>, res: Response<User>) {
+                        Log.d("EDIT PROFILE", "onResponse: ${req.request().body}")
+                        repository.user.value = res.body()
+                    }
+
+                    override fun onFailure(req: Call<User>, t: Throwable) {
+                        Toast.makeText(
+                            con,
+                            "Erro ao atualizar: tente novamente mais tarde",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                })
+        }
+        FirebaseAuth.getInstance().currentUser?.updateProfile(
+            userProfileChangeRequest {
+                displayName = username
+                photoUri = image
+            }
+        )
+
+
+    }
+
+}
