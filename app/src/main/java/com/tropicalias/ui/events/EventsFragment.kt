8cab137d3c92a636df55c516449cf79e5ad34262
nicolasruoco <nicolas.ruoco@germinare.org.br>
2024-10-17 08:30:39 -0300

    feat: creating a fragment for the event

--- app/src/main/java/com/tropicalias/ui/events/EventsFragment.kt
+++ app/src/main/java/com/tropicalias/ui/events/EventsFragment.kt
@@ -5,35 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.tropicalias.MainViewModel
 import com.tropicalias.databinding.FragmentEventsBinding
 
 class EventsFragment : Fragment() {
 
     private var _binding: FragmentEventsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val viewModel: EventsViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val eventsViewModel =
-            ViewModelProvider(this).get(EventsViewModel::class.java)
-
         _binding = FragmentEventsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        eventsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
